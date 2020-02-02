d1e632574b733953904782836e21a48ba20e702c
David <781875815@qq.com>
2020-02-02 20:19:08 +0800

    Update UserDao.java

--- src/com/dao/UserDao.java
+++ src/com/dao/UserDao.java
@@ -11,11 +11,4 @@ public interface UserDao {
 	public boolean delete(int id) ;//根据id删除用户
 	public boolean update(int id,String name, String pwd,String sex, String home,String info) ;//更新用户信息
 
-	
-	//sdfas
-	//git test;
-
-	//adfas
-
-// asdasdasdasdasdasdas
 }
