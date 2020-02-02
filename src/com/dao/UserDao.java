ea37daaf37950addb0e5aff6a6695c3b34167291
huangliqwe2020 <59169297+huangliqwe2020@users.noreply.github.com>
2020-02-02 20:01:41 +0800

    Update UserDao.java

--- src/com/dao/UserDao.java
+++ src/com/dao/UserDao.java
@@ -5,12 +5,14 @@ import java.util.List;
 import com.entity.User;
  
 public interface UserDao {
-	public boolean login(String name,String pwd);//登录
-	public boolean register(User user);//注册
-	public List<User> getUserAll();//返回用户信息集合
-	public boolean delete(int id) ;//根据id删除用户
-	public boolean update(int id,String name, String pwd,String sex, String home,String info) ;//更新用户信息
+	public boolean login(String name,String pwd);//鐧诲綍
+	public boolean register(User user);//娉ㄥ唽
+	public List<User> getUserAll();//杩斿洖鐢ㄦ埛淇℃伅闆嗗悎
+	public boolean delete(int id) ;//鏍规嵁id鍒犻櫎鐢ㄦ埛
+	public boolean update(int id,String name, String pwd,String sex, String home,String info) ;//鏇存柊鐢ㄦ埛淇℃伅
 
 	
 	//sdfas
+	//git test;
+
 }
